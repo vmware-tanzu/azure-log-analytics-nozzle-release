63b60e092f5b1aa1de0f1911619ef23202c40d1a
Kaitlin Barrer <kbarrer@vmware.com>
2020-11-16 23:09:01 +0000

    add osstp script

---
+++ hack/osstp.sh
@@ -0,0 +1,13 @@
+# Checkout to shas version used in pks. You can find release version in p-pks-integrations
+# use sink-resources release to find the version of the images which should be the same
+# as the git shas of the repo
+BASE_DIR="$(cd $(dirname $0)/.. && pwd -P)"
+OSSTP_LOAD=${OSSTP_LOAD:-~/workspace/osstpclients/bin/osstp-load.py}
+OSM_API_KEY_FILE=${OSM_API_KEY_FILE:-~/.osmapikey}
+REMAPPER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/osstp.yaml
+
+pushd $BASE_DIR
+    osstptool generate --vendor-path=vendor azure-nozzle github.com/vmware-tanzu/nozzle-for-microsoft-azure-log-analytics --remapper-definitions-file=$BASE_DIR/hack/osstp.yaml
+    osstptool download
+    python2 $OSSTP_LOAD -R azure-nozzle/2.0.0 osstp_golang.yml -I "Distributed - Static Link w/ TP" -A $OSM_API_KEY_FILE
+popd
